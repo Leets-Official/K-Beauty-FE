a5b17b193ea7bc01ce9386e9b520141247e68883
sd091415 <sd091415@gamil.com>
2026-07-25 22:21:03 +0900

    [STYLE] #25 로고 마크 추가

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,2 +1,3 @@
+export { default as CosmetchMarkIcon } from './cosmetch_mark.svg?react';
 export { default as ProductTonerIcon } from './product_toner.svg?react';
 export { default as ProductSerumIcon } from './product_serum.svg?react';
