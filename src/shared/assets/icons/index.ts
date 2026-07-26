36c7d52f912c3a399327456454022a3a34cecdfd
sd091415 <sd091415@gamil.com>
2026-07-26 16:39:44 +0900

    [STYLE] #27 토스트 메시지에 사용할 아이콘 추가

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,2 +1,4 @@
+export { default as AlertCircleIcon } from './alert_circle.svg?react';
+export { default as CheckCircleIcon } from './check_circle.svg?react';
 export { default as ProductTonerIcon } from './product_toner.svg?react';
 export { default as ProductSerumIcon } from './product_serum.svg?react';
