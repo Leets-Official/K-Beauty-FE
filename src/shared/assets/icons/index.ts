b30386945466241b542da8fe4821fcb389a547e3
sd091415 <sd091415@gamil.com>
2026-07-28 00:34:20 +0900

    [STYLE] #30 아이콘 추가

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as ProductTonerIcon } from './product_toner.svg?react';
 export { default as ProductSerumIcon } from './product_serum.svg?react';
+export { default as NoticeAlertIcon } from './notice_alert.svg?react';
+export { default as UnassessedInfoIcon } from './unassessed_info.svg?react';
