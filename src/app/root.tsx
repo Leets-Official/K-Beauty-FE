a85433988489e6c7e57feeb42ba5b236c67caddf
wo-gml <jo78475@gmail.com>
2026-07-30 02:13:38 +0900

    [FIX] #24 루트 레이아웃에 Links 및 Meta 추가

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Scripts, ScrollRestoration } from 'react-router';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
 
 import './styles/global.css';
 
@@ -9,6 +9,8 @@ export default function Root() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>K-Beauty</title>
+        <Meta />
+        <Links />
       </head>
 
       <body>
