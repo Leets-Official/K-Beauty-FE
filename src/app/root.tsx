360813420ac7fd8be73a7dba673677d1b5da7b05
Dahyeon <dahhyeon22@gmail.com>
2026-08-03 19:16:48 +0900

    [CHORE] #33 파비콘 설정

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -11,7 +11,8 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>K-Beauty</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <title>Cosmetch</title>
         <Meta />
         <Links />
       </head>
